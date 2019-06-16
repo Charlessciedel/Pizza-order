1fe71bfa1b05845bf04c74e458446f9b99c22981
George Charles <charlesschiedel@gmail.com>
2019-06-17 02:26:01 +0300

    I have added images to the order.html

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,8 @@
-$('.carousel-3d-autoplay-off').mdbCarousel3d({
+ 
+ $('.carousel-3d-autoplay-off').mdbCarousel3d({
     autoplay: false
   });
+  $('.card').hover(function () {
+    $(this).children('.card-1').fadeToggle(1000, 'linear');
+  });
+  $('*').hide();
