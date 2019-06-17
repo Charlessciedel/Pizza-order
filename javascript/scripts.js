1f0a29ca51685ec32685de3925311524952514ea
George Charles <charlesschiedel@gmail.com>
2019-06-17 15:08:51 +0300

    I have removed the order.html

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,12 +5,49 @@
   $('.card').hover(function () {
     $(this).children('.card-1').fadeToggle(1000, 'linear');
   });
- function Pizza (size, stuffing,topping){
-   this.size= size;
-   this.stuffing = stuffing;
-   this.topping = topping;
- };
+ 
+$("#large").click(function disable() {
+  document.getElementById("medium", "small").disabled = true;
+
+});
+$("#medium").click(function disable() {
+  document.getElementById("large", "small").disabled = true;
+
+});
+$("#small").click(function disable() {
+  document.getElementById("medium", "large").disabled = true;
+
+});
+$("#crispy").click(function disable() {
+  document.getElementById("crusty", "gluten-free").disabled = true;
+
+});
+$("#crusty").click(function disable() {
+  document.getElementById("crispy", "gluten-free").disabled = true;
+
+});
+$("#gluten-free").click(function disable() {
+  document.getElementById("crispy", "crusty").disabled = true;
+
+});
+function Pizza (size, stuffing,topping){
+  this.size= size;
+  this.stuffing = stuffing;
+  this.topping = topping;
+};
+var large= getElementById("#large");
+var medium= getElementById("#medium");
+var small= getElementById("#small")
 let sizes=[ "Large", "Medium", "Small"];
 let stuffings= [ "Crispy", "Crusty", "Gluten-Free"];
-let toppings= [ "Onions", "Bacons", "Black-Olive", "Pepperoni", "Sausage", "Mushrooms"];
-si
+let toppings= [ "Onions", "Bacons", "Black-Olive", "Pepperoni", "Sausage", "Mushrooms"]
+
+for(i=0; i<array.length; i++){
+  document.getElementById(array[i]).value = array[i];
+  document.getElementById(array[i]).innerHTML = array[i];
+}
+if ( sizes.indexOf( 0 ) === 0 ) {
+  
+
+  
+}
