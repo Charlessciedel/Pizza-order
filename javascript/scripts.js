e8ab2be3b6af06c4340c233f820abd2c59b08838
George Charles <charlesschiedel@gmail.com>
2019-06-17 09:16:52 +0300

    I have added order items

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,4 +5,8 @@
   $('.card').hover(function () {
     $(this).children('.card-1').fadeToggle(1000, 'linear');
   });
-  $('*').hide();
+ // SideNav Button Initialization
+$(".button-collapse").sideNav();
+// SideNav Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+var ps = new PerfectScrollbar(sideNavScrollbar);
