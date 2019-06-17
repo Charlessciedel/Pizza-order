4d284dcba84abfd38449d60759c6c6f6c3535394
George Charles <charlesschiedel@gmail.com>
2019-06-17 16:14:20 +0300

    I have modified my scripts.js and my html

---
+++ README.md
@@ -0,0 +1,85 @@
+
+# A website to order and track pizza orders
+
+#### 17th June,2019
+
+#### By George Schiedel
+
+## Description
+
+A website to order and track pizza orders. Users should be able to order more than one pizza, only get prompted to enter an address if they are ordering any one of their pizzas for delivery, navigate _somewhat_ intuitively between address submission, checkout, and the order form.
+
+### Specifications
+
+|Behavior|Input|Output|
+|:---:|:---:|:---:|
+|User selects a topping|User selects topping|Topping selection returned|
+|Users selects more than one topping|User selects more than one topping|More than one topping returned|
+|Selected toppings are unique|User selects n toppings|n|
+|User selects size of the pizza|User selects large|Large returned|
+|Price of pizza is calculated based on size of pizza and number of toppings. Toppings beyond the first are slightly cheaper than the first topping|User selects small pizza with three toppings|$11.60|
+|Meat toppings cost slightly more than veggie toppings|User selects small pizza with three meat toppings|$12.80|
+|Users can order more than one pizza|User selects more than one pizza|Second (or more) pizza purchases tracked|
+|Costs for more than one pizza are added to price of first pizza|User orders more than one pizza|Cumulative price returned|
+|User selects a delivery option|Delivery selected|Delivery option returned|
+|Cost of delivery is added to price of the pizza|Delivery selected|New cumulative price reported|
+
+## Setup/Installation Requirements
+
+* This repository is meant to be viewed. It can be viewed [here](https://Atticus29.github.io/pizzaOrder).
+
+### Or if you're feeling bold, you can clone OR download a local instance of the site:
+
+* Clone
+  * Open your terminal program
+    * On a Mac, this would be in the Applications/Utilities directory, and is called, "Terminal"
+    * Windows uses a Terminal program as well, but a Terminal with all the capabilities we'll require is not installed by default. Thankfully, we can easily download and install a Terminal program that does fit our needs.
+There are many options available, but we recommend using a terminal program called git bash. You can download this free program at 
+  * Clone this track survey repository by typing, `git clone https://github.com/Atticus29/pizzaOrder.git`
+* Download
+  * Click [here] to download the repo
+  * Unzip the zipped repository
+* Open the pizzaOrder folder and double-click on index.html.
+* Make your selections and click submit as instructed on the site.
+
+
+## Known Bugs
+
+There is currently no way to remove orders, and sometimes the only way to get to checkout again is to go through the order page again, thereby forcing you to buy a new pizza. Ran out of time to correct this.
+
+## Support and contact details
+
+
+
+## Technologies Used
+
+
+* html5
+* bootstrap v. 3.3.7
+* CSS
+* javaScript
+* jQuery v. 3.1.1
+
+### License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2018 George Charles
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,240 @@
+// Back End
+
+// containsObject was stolen from the internet
+function containsObject(obj, list) {
+  var i;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function Address(name, street, city, state, zip){
+  this.name = name;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+}
+
+Address.prototype.reportAddress = function(){
+  return (this.name + "<br>" + this.street + "<br>" + this.city + ", " + this.state + " " + this.zip);
+}
+
+function Pizza (size, numVeggies, numMeats, deliveryStatus){
+  this.size = size;
+  this.numVeggies = numVeggies;
+  this.numMeats = numMeats;
+  this.deliveryStatus = deliveryStatus;
+}
+
+Pizza.prototype.isValidSize = function(){
+  if((typeof this.size === "string") && (this.size === "Large" || this.size === "Medium" || this.size === "Small")){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+Pizza.prototype.isValidNumVeggies = function(){
+  if((typeof this.numVeggies === "number") && (this.numVeggies >= 0) && (Math.trunc(this.numVeggies) === this.numVeggies)){
+    return true;
+  } else{    return false;
+  }
+}
+
+Pizza.prototype.isValidNumMeats = function(){
+  if((typeof this.numMeats === "number") && (this.numMeats >= 0) && (Math.trunc(this.numMeats) === this.numMeats)){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+Pizza.prototype.reportOrderBasic = function(){
+  return "1 " + this.size + " pizza";
+}
+
+Pizza.prototype.reportOrderVerbose = function(){
+  if(this.deliveryStatus === true){
+    return this.size + " pizza with " + this.numVeggies + " vegetable topping(s) and " + this.numMeats + " meat topping(s) for delivery = $" + this.calculatePrice();
+  } else{
+    return (this.size + " pizza with " + this.numVeggies + " vegetable toppings and " + this.numMeats + " meat toppings for pickup = $" + this.calculatePrice());
+  }
+}
+
+Pizza.prototype.calculatePrice = function (){
+  var price = 0;
+  // validate
+  if(this.isValidSize() && this.isValidNumVeggies() && this.isValidNumMeats()){
+    // Add size to price
+    if(this.size === "Large"){
+      price += 13;
+    } else if (this.size === "Medium"){
+      price += 11.50;
+    } else {
+      price += 10;
+    }
+    // Add veggie toppings to price
+    for (var i = 1; i <= this.numVeggies; i++){
+      if(i === 1){
+        price += 0.5;
+      } else{
+        price += 0.3;
+      }
+    }
+    // Add meat toppings to price
+    for (var i = 1; i <= this.numMeats; i++){
+      if(i === 1){
+        price += 1;
+      } else{
+        price += 0.9;
+      }
+    }
+    if(this.deliveryStatus){
+      var deliveryPrice = 10;
+      price += deliveryPrice;
+    }
+    var rounded = (Math.round(price*100))/100;
+    return (rounded.toFixed(2));
+  } else{
+  }
+}
+
+var testOrders = function(){
+  var pizza1 = new Pizza("Large", 1, 1, false);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Large", 1, 1, true);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Large", 1, 2, false);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Small", 1, 1, false);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Medium", 2, 1, false);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Medium", 5, 1, false);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Medium", 20, 20, false);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Large", 100, 100, true);
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Large", 1.5, 1, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Large", 1, 1.5, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Large", 1, 1.5, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Any", 1, 1, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Medium", 1, 1, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("medium", -1, 1, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Medium", -1, 1, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("medium", 1, -3, false); // should throw error
+  console.log(pizza1.calculatePrice());
+  var pizza1 = new Pizza("Medium", 1, -3, false); // should throw error
+  console.log(pizza1.calculatePrice());
+}
+
+testOrders();
+
+// Make deliveryPrice a function
+
+// Front End
+$(function(){
+  var checkoutBag = [];
+  var replacementTracker = [];
+  $("#pizzaForm").submit(function(){
+    event.preventDefault();
+    var sizeInput = $("input:radio[name=size]:checked").val();
+    var veggieInputArray = []
+    $("input:checkbox[name=veggie]:checked").each(function(){
+      veggieInputArray.push($(this).val());
+    })
+    var meatInputArray = [];
+    $("input:checkbox[name=meat]:checked").each(function(){
+      meatInputArray.push($(this).val());
+    })
+    var deliveryStatus = (($("input:radio[name=service]:checked").val()) === "true");
+    var currentPizza = new Pizza (sizeInput, veggieInputArray.length, meatInputArray.length, deliveryStatus);
+    if(currentPizza.deliveryStatus){
+      $("#addressBtn").show();
+    }
+    checkoutBag.push(currentPizza);
+    $("#pizzaForm").hide();
+    $("#orderAgain").show();
+    $("#results").show();
+    $("#order-summary").empty();
+    $(".grand-toal").remove();
+    checkoutBag.forEach(function(pizza){
+      $("#order-summary").append("<li><span class='pizza-item'>" + pizza.reportOrderBasic()+ "</span></li>");
+    });
+  });
+
+  $("#addressBtn").click(function(){
+    $("#address-form").show();
+    $("#results").hide();
+  });
+
+  $("#address-form").submit(function(){
+    event.preventDefault();
+    var nameInput = $("#usrName").val();
+    var streetInput = $("#usrStreet").val();
+    var cityInput = $("#usrCity").val();
+    var stateInput = $("#usrState").val();
+    var zipInput = $("#usrZip").val();
+    currentAddress = new Address(nameInput, streetInput, cityInput, stateInput, zipInput)
+    $("#address-form").hide();
+    $("#addressBtn").remove();
+    $("#orderAgain").show();
+  });
+
+  $("#orderAgainBtn").click(function(){
+    event.preventDefault();
+    $("#pizzaForm").show();
+    $("#orderAgain").hide();
+    $("#results").hide();
+  });
+
+  $("#checkout-btn").click(function(){
+    event.preventDefault();
+    $("#checkout-btn").hide();
+    $("#backBtn").show();
+    $("#pizzaForm").hide();
+    $("#orderAgain").hide();
+    $("#results").show();
+    $("#order-summary").empty();
+    checkoutBag.forEach(function(pizza){
+      $("#order-summary").append("<li><span class='pizza-item'>" + pizza.reportOrderBasic()+ "</span></li>");
+    });
+    $("#order-summary").append("<hr>");
+    var grandTotal = 0;
+    checkoutBag.forEach(function(pizza){
+      grandTotal += parseFloat(pizza.calculatePrice());
+    })
+    $(".grand-total").remove();
+    $("#order-summary").after("<p class='grand-total'>Grand total is: $ " + grandTotal.toFixed(2) + ".</p>");
+    $(".address-report").remove();
+    $(".grand-total").after("<p class='address-report'>Order will be sent to:<br>" + currentAddress.reportAddress() + "</p>");
+  });
+
+  $("#backBtn").click(function(){
+    event.preventDefault();
+    $("#backBtn").hide();
+    $("#orderAgain").show();
+    $("#checkout-btn").show();
+  });
+
+  $("#order-summary").last().on("click", ".pizza-item", function(){
+    event.preventDefault();    var checkoutBagIndexToGet = $(".pizza-item").index($(this));
+    var replacement = checkoutBag[checkoutBagIndexToGet].reportOrderVerbose();
+    if(!containsObject(replacement, replacementTracker)){
+      $("#detailed-ordered-items").append("<li class='pizza-extended'>" + replacement + "</li>")
+    }
+    replacementTracker.push(replacement);
+  });
+});
